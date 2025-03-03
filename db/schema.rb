bfa1896e84fbdcfbb503f46f6408229c88e99773
Gipnokot <shootinputin007@gmail.com>
2025-03-03 21:13:27 +0500

    Add index for name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_03_120722) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_03_160715) do
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 100, null: false
     t.string "email", limit: 150, null: false
@@ -18,5 +18,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_03_120722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 end
